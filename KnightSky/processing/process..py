7086e24945662ea192d64b9a169c8a7f597fe854
Aubhro Sengupta <aubhrosengupta@gmail.com>
2017-07-27 11:19:46 -0700

    Remove move numbers from games database Use regex to remove numbers and save a file of only moves in algebraic notation.

--- KnightSky/processing/process..py
+++ KnightSky/processing/process..py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 original_data = "ficsgamesdb.pgn"
@@ -10,9 +11,23 @@ processed_path = os.path.join(os.pardir, os.pardir, processed_data)
 
 print(processed_path)
 
-forfeit = "forfeits by disconnection"
-with open(original_path, 'r') as raw, open(processed_path, 'w+') as processed:
-    for line in raw:
-        if line[:2] == "1." and forfeit not in line:
-            end = line.index('{')
-            processed.write(line[:end] + '\n')
+
+def remove_metadata():
+
+    forfeit = "forfeits by disconnection"
+    with open(original_path, 'r') as raw, open(processed_path, 'w+') as processed:
+        for line in raw:
+            if line[:2] == "1." and forfeit not in line:
+                end = line.index('{')
+                processed_line = line[:end].replace('+', '').replace('#', '') + '\n'
+                processed_line = re.sub(r'\d\.\s', '', processed_line)
+                processed.write(processed_line)
+
+
+def convert_to_bitmap():
+    with open(processed_path, 'r') as processed:
+        for line in processed:
+            move_list = line.split(' ')
+
+if __name__ == '__main__':
+    remove_metadata()
