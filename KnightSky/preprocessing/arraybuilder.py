fb30df5331213094f98a17c24c6da1da62f7abb1
Aubhro <aubhrosengupta@gmail.com>
2017-09-14 22:59:41 -0700

    Separate processing functions into different modules Create preprocessing directory and helpers

--- KnightSky/preprocessing/arraybuilder.py
+++ KnightSky/preprocessing/arraybuilder.py
@@ -5,23 +5,25 @@ import numpy as np
 import re
 import itertools
 import random
-
 from chess_py import *
+
+from KnightSky.helpers import oshelper
 from KnightSky.vector import bitmap
 
 
-original_path = os.path.abspath("../data/raw")
+""" Defines paths to raw data, processed data, and numpy arrays.
+Creates directories if they do not exist. """
+oshelper.create_if_not_exists("../data")
+
+original_path = oshelper.abspath("../data/raw")
 print("raw data in {}".format(original_path))
 
-processed_path = os.path.abspath("../data/processed")
-processed_filename = "processed.pgn"
-if not os.path.exists(processed_path):
-    os.makedirs(processed_path)
+processed_path = os.path.abspath("../data/processed/processed.pgn")
+oshelper.create_if_not_exists(processed_path)
 print("Processed data to be placed in {}".format(processed_path))
 
 arraypath = os.path.abspath("../data/arrays")
-if not os.path.exists(arraypath):
-    os.makedirs(arraypath)
+oshelper.create_if_not_exists(arraypath)
 print("Arrays to be placed in {}".format(arraypath))
 
 
@@ -38,7 +40,7 @@ def _remove_metadata(path):
 
     forfeit = "forfeits by disconnection"
     with open(path, 'r') as raw, \
-            open(os.path.join(processed_path, processed_filename), 'w+') as processed:
+            open(processed_path, 'w+') as processed:
         for line in raw:
 
             if line[:2] == "1." and forfeit not in line: # Game is there
@@ -66,7 +68,7 @@ def convert_to_arrays():
     """
     bitmap_X, bitmap_y = [], []
     color_dict = {color.white: 0, color.black: 1}
-    with open(os.path.join(processed_path, processed_filename), 'r') as processed:
+    with open(processed_path, 'r') as processed:
 
         for i, line in enumerate(processed):
 
@@ -130,41 +132,11 @@ def convert_to_arrays():
 
     bitmap_X = np.array(bitmap_X)
     bitmap_y = np.array(bitmap_y)
-    xpath = os.path.join(arraypath, 'bitmap_X')
-    ypath = os.path.join(arraypath, 'bitmap_y')
 
-    if os.path.exists(xpath):
-        os.remove(xpath)
-    if os.path.exists(ypath):
-        os.remove(ypath)
-
-    np.save(os.path.join(arraypath, 'bitmap_X'), bitmap_X)
-    np.save(os.path.join(arraypath, 'bitmap_y'), bitmap_y)
+    np.save(oshelper.pathjoin(arraypath, 'bitmap_X'), bitmap_X)
+    np.save(oshelper.pathjoin(arraypath, 'bitmap_y'), bitmap_y)
 
     return bitmap_X, bitmap_y
-
-
-def randomly_assign_train_test(X_data, y_data, test_size=0.1):
-    data = list(zip(list(X_data), list(y_data)))
-    random.shuffle(data)
-
-    shuffled_X, shuffled_y = zip(*data)
-
-    X_split_index = int(len(shuffled_X) * test_size)
-    y_split_index = int(len(shuffled_y) * test_size)
-
-    X_train, X_test = shuffled_X[X_split_index:], shuffled_X[:X_split_index]
-    y_train, y_test = shuffled_y[y_split_index:], shuffled_y[:y_split_index:]
-    return X_train, X_test, y_train, y_test
-
-
-def next_batch(X, y, batch_size=100):
-    n_batches = int(len(X) / batch_size)
-
-    for batch in range(n_batches):
-        start = (batch * batch_size)
-        end = start + batch_size if start + batch_size < len(X) else len(X) - 1
-        yield X[start:end], y[start:end]
 
 
 if __name__ == '__main__':
