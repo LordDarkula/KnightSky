b9a10c65a482c50c1f9ca2eea7f1580f33b6293b
Aubhro <aubhrosengupta@gmail.com>
2018-02-14 13:49:07 -0800

    Reorder code and rename tensorboard methods

--- KnightSky/models/cnn/helpers/tensorboardsetup.py
+++ KnightSky/models/cnn/helpers/tensorboardsetup.py
@@ -11,35 +11,36 @@ from KnightSky.helpers import oshelper
 
 def current_run_directory(tmp_path, run_name='knight'):
     """
-    Sets correct tensorboard directory dynamically based on number of runs.
+    Returns correct tensorboard directory which is set dynamically based on number of runs.
     Delete tensorboard directory to start count over
     """
     runpath = oshelper.pathjoin(tmp_path, run_name)
     print(runpath)
     oshelper.create_if_not_exists(runpath)
-    count = _incr_tb_run_number(runpath)
-    return oshelper.pathjoin(runpath, count)
+    count = _increment_tb_run_number(runpath)
+    return oshelper.pathjoin(runpath, str(count))
 
 
-def _incr_tb_run_number(runpath):
+def _increment_tb_run_number(runpath):
     """
     Increments tensorboard count by 1 for the new run. If no runs are present, create the directory itself.
-    :param tbpath: path of tensorboard
+    :param runpath: path of tensorboard
     :return: The number of runs plus 1 for use as the name of the new run
     """
     countfilename = oshelper.pathjoin(runpath, 'count.txt')
-    if not os.path.exists(countfilename):
-
-        with open(countfilename, 'w') as f:
-            f.write('1')
-
-        return '1'
-
-    else:
+    if os.path.exists(countfilename):
+        # Open count.txt where number of runs are stored
         with open(countfilename, 'r') as f:
             current = int(f.readline())
 
         with open(countfilename, 'w') as f:
             f.write(str(current + 1))
 
-        return str(current + 1)
+        return current + 1
+
+    else:
+        # Create new file file to store tensorboard runs if none exists
+        with open(countfilename, 'w') as f:
+            f.write('1')
+
+        return 1
