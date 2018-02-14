8d6f8a58fba68effc73be180a818d72d82040792
Aubhro Sengupta <aubhrosengupta@gmail.com>
2018-02-13 23:41:01 -0800

    Rename data splitting intermediate variables to remove capital X and y

--- KnightSky/preprocessing/split.py
+++ KnightSky/preprocessing/split.py
@@ -3,27 +3,27 @@
 import random
 
 
-def randomly_assign_train_test(X_data, y_data, test_size=0.1):
-    data = list(zip(list(X_data), list(y_data)))
+def randomly_assign_train_test(features, labels, test_size=0.1):
+    data = list(zip(list(features), list(labels)))
     random.shuffle(data)
 
     # Unzips data
-    shuffled_X, shuffled_y = zip(*data)
+    shuffled_features, shuffled_labels = zip(*data)
 
-    X_split_index = int(len(shuffled_X) * test_size)
-    y_split_index = int(len(shuffled_y) * test_size)
+    split_index = int(len(shuffled_features) * test_size)
 
-    X_train, X_test = shuffled_X[X_split_index:], shuffled_X[:X_split_index]
-    y_train, y_test = shuffled_y[y_split_index:], shuffled_y[:y_split_index]
-    return X_train, X_test, y_train, y_test
+    train_features, test_features = shuffled_features[split_index:], shuffled_features[:split_index]
+    train_labels, test_labels = shuffled_labels[split_index:], shuffled_labels[:split_index]
+    return train_features, test_features, train_labels, test_labels
 
 
-def next_batch(X, y, batch_size=100):
-    number_of_batches = int(len(X) / batch_size)
+def next_batch(features, labels, batch_size=100):
+    number_of_batches = int(len(features) / batch_size)
 
     for batch in range(number_of_batches):
         start_index = (batch * batch_size)
         end_index = start_index + batch_size \
-            if start_index + batch_size < len(X) \
-            else len(X) - 1
-        yield X[start_index:end_index], y[start_index:end_index]
+            if start_index + batch_size < len(features) \
+            else len(features) - 1
+
+        yield features[start_index:end_index], labels[start_index:end_index]
